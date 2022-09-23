136caebb4b37e2af179712b3df3f3bfdd04fd382
Patty <patriciamae@1export.com>
2022-09-23 15:53:35 +0800

    Computed Properties

--- app.js
+++ app.js
@@ -1,15 +1,29 @@
 new Vue({
     el: '#vue-app',
     data:{
-        name:'',
-        age:'',
+       age:20,
+       a:0,
+       b:0,
     }, 
     methods: {
-      logName: function() {
-        this.name = "";
+    //Comment this method to run the computed property, still same functionality
+     /* addToA:function(){
+        console.log('addToA');
+        return this.a + this.age;
       },
-      logAge: function() {
-        console.log('you entered your age')
+      addToB:function(){
+        console.log('addToB');
+        return this.b + this.age;
+      }*/
+    },
+    computed: {
+      addToA:function(){
+        console.log('addToA');
+        return this.a + this.age;
       },
+      addToB:function(){
+        console.log('addToB');
+        return this.b + this.age;
+      }
     }
 });
