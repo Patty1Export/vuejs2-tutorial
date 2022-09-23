ca7e4de7197df28770e759e183931123b09674fd
Patty <patriciamae@1export.com>
2022-09-23 12:00:56 +0800

    Keyboard Events

--- app.js
+++ app.js
@@ -1,27 +1,14 @@
 new Vue({
     el: '#vue-app',
     data:{
-        name:'Shaun',
-        job:'Ninja',
-        age:25,
-        website:"www.thenetninja.co.uk", // Add data website property
-        websiteTag:'<a href="www.thenetninja.co.uk"> The Net Ninja Website</a>',
-        x:0,
-        y:0,
-    },  //store all the data in vue instance
+        
+    }, 
     methods: {
-       add:function(inc) {
-        this.age += inc;
-       },
-       subtract:function(dec){
-        this.age -= dec;
-       },
-       updateXY:function(event){
-        this.x =event.offsetX;
-        this.y =event.offsetY;
-       },
-       click:function(){
-        alert('You clicked me');
-       }
+      logName: function() {
+        console.log('you entered your name')
+      },
+      logAge: function() {
+        console.log('you entered your age')
+      },
     }
 });
