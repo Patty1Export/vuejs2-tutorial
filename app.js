c460d3eb71b7ab55d4ebc9983fd5c19a1b94640a
Patty <patriciamae@1export.com>
2022-09-23 10:54:59 +0800

    Add double click event

--- app.js
+++ app.js
@@ -8,11 +8,11 @@ new Vue({
         websiteTag:'<a href="www.thenetninja.co.uk"> The Net Ninja Website</a>'
     },  //store all the data in vue instance
     methods: {
-        greet: function(time) {
-            
-            return `Good ${time} ${this.name}` 
-            //change time using passed  parameters
-            //add return name property in function method greet to access the data
-        }
+       add:function(inc) {
+        this.age += inc;
+       },
+       subtract:function(dec){
+        this.age -= dec;
+       }
     }
 });
