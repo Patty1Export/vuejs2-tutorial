4e62c64727cd876cd2c7a91ceb517f081baf6184
Patty <114051644+Patty1Export@users.noreply.github.com>
2022-09-25 17:40:56 +0800

    Show available class

--- app.js
+++ app.js
@@ -1,29 +1,13 @@
 new Vue({
     el: '#vue-app',
     data:{
-       age:20,
-       a:0,
-       b:0,
+       available:true,
+       nearby:false, 
     }, 
     methods: {
-    //Comment this method to run the computed property, still same functionality
-     /* addToA:function(){
-        console.log('addToA');
-        return this.a + this.age;
-      },
-      addToB:function(){
-        console.log('addToB');
-        return this.b + this.age;
-      }*/
+   
     },
     computed: {
-      addToA:function(){
-        console.log('addToA');
-        return this.a + this.age;
-      },
-      addToB:function(){
-        console.log('addToB');
-        return this.b + this.age;
-      }
+
     }
 });
