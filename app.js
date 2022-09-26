dd0745616c40bf9f6e61bc123a51872acfde1077
Patty <114051644+Patty1Export@users.noreply.github.com>
2022-09-26 11:06:45 +0800

    Use v-for directive to display strings and objects

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 new Vue({
     el: '#vue-app',
     data:{
-      error: false,
-      success: false,
+      name:'jack',
+      characters:['Mario', 'Luigi', 'Yoshi', 'Bowser'], //array of strings
+      ninjas:[ //array of objects
+        {name:'Ryu', age:25},
+        {name:'Yoshi', age:35},
+        {name:'Ken', age:55},
+      ]
     }, 
     methods: {
    
